ca7071d219c4ef66a245cf4aa62bfb7a82f804a7
zgf123 <277845885@qq.com>
2018-06-21 06:17:38 +0000

    add scaffold

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -2,8 +2,16 @@
 
 use Faker\Generator as Faker;
 
+
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+    $updated_at = $faker->dateTimeThisMonth();
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+    
     return [
-        // 'name' => $faker->name,
+        'title' => $faker->sentence(),
+        'body' => $faker->text(),
+        'excerpt' => $faker->sentence(),
+        'updated_at' => $updated_at,
+        'created_at' => $created_at,
     ];
 });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = app(Faker\Generator::class);
+        $avatars = [
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+
+        $users = factory(User::class)->times(10)->make()->each(function ($user, $index) use ($faker, $avatars){
+            $user->avatar = $faker->randomElement($avatars);
+        });
+
+        $users_array = $users->makeVisible(['password', 'remember_token']);
+
+        User::insert($users_array->toArray());
+
+
+        $user = User::first();
+        $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200';
+        $user->name = 'dai';
+        $user->email = '277845885@qq.com';
+        $user->password = bcrypt('123123');
+        $user->save();
+    }
+}
