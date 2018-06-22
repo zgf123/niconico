bd8e701e408b240b6c0792f1b4e8ba289b482a63
zgf123 <277845885@qq.com>
2018-06-22 14:59:00 +0000

    baidu slug

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -23,8 +23,12 @@ class TopicsController extends Controller
 		return view('topics.index', compact('topics'));
 	}
 
-    public function show(Topic $topic)
+    public function show(Topic $topic, Request $request)
     {
+		if( isset($topic->slug) && $topic->slug != $request->slug ){
+			return redirect($topic->link(), 301);
+		}
+		
         return view('topics.show', compact('topic'));
     }
 
@@ -40,7 +44,7 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
         $topic->save();
 
-        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+        return redirect()->to($topic->link())->with('message', 'Created successfully.');
     }
 
 	public function edit(Topic $topic)
@@ -55,7 +59,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->to($topic->link())->with('message', 'Updated successfully.');
 	}
 
 	public function destroy(Topic $topic)
--- resources/views/topics/_topic_list.blade.php
+++ resources/views/topics/_topic_list.blade.php
@@ -4,7 +4,7 @@
         @foreach ($topics as $topic)
             <li class="media">
                 <div class="media-left">
-                    <a href="{{ route('users.show', [$topic->user_id]) }}">
+                    <a href="{{ $topic->link() }}">
                         <img class="media-object img-thumbnail" style="width: 52px; height: 52px;" src="{{ $topic->user->avatar }}" title="{{ $topic->user->name }}">
                     </a>
                 </div>
@@ -12,10 +12,10 @@
                 <div class="media-body">
 
                     <div class="media-heading">
-                        <a href="{{ route('topics.show', [$topic->id]) }}" title="{{ $topic->title }}">
+                        <a href="{{ $topic->link() }}" title="{{ $topic->title }}">
                             {{ $topic->title }}
                         </a>
-                        <a class="pull-right" href="{{ route('topics.show', [$topic->id]) }}" >
+                        <a class="pull-right" href="{{ $topic->link() }}" >
                             <span class="badge"> {{ $topic->reply_count }} </span>
                         </a>
                     </div>
@@ -28,7 +28,7 @@
                         </a>
 
                         <span> • </span>
-                        <a href="{{ route('users.show', [$topic->user_id]) }}" title="{{ $topic->user->name }}">
+                        <a href="{{ $topic->link() }}" title="{{ $topic->user->name }}">
                             <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
                             {{ $topic->user->name }}
                         </a>
